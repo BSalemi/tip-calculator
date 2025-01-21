fb192b38182c28b823d3ec80bb995c846f7ab945
BSalemi <briandsalemi@gmail.com>
2025-01-21 07:44:12 -0500

    added state to Service select and option tags

--- src/Service.js
+++ src/Service.js
@@ -1,14 +1,15 @@
 import { useState } from "react";
 
 function Service() {
+  const [tip, setTip] = useState(5);
   return (
     <div>
       <span>How did you like your service?</span>
-      <select>
-        <option> Dissatisfied (0%)</option>
-        <option> It was okay (5%)</option>
-        <option> It was good (10%)</option>
-        <option> Absolutely amazing! (20%)</option>
+      <select value={tip}>
+        <option value={0}> Dissatisfied (0%)</option>
+        <option value={5}> It was okay (5%)</option>
+        <option value={10}> It was good (10%)</option>
+        <option value={20}> Absolutely amazing! (20%)</option>
       </select>
     </div>
   );
