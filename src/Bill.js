62592aca62e6554f8b617df9964a15b01456f9a0
BSalemi <briandsalemi@gmail.com>
2025-01-21 07:24:09 -0500

    added event handler to input field to update state

--- src/Bill.js
+++ src/Bill.js
@@ -2,10 +2,15 @@ import { useState } from "react";
 
 function Bill() {
   const [price, setPrice] = useState("");
+
   return (
     <div>
       <span>How much was the bill?</span>
-      <input type="text"></input>
+      <input
+        type="text"
+        value={price}
+        onChange={(e) => setPrice(e.target.value)}
+      ></input>
     </div>
   );
 }
