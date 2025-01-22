a564727bbd8be75f0b1cc608c253530f2c480021
BSalemi <briandsalemi@gmail.com>
2025-01-22 06:27:57 -0500

    added event handler to Service component and created the ResetButton component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Bill from "./Bill.js";
 import Service from "./Service.js";
+import Reset from "./ResetButton.js";
 
 function App() {
   return (
     <div>
       <Bill />
       <Service />
+      <Reset />
     </div>
   );
 }
--- src/Service.js
+++ src/Service.js
@@ -2,10 +2,11 @@ import { useState } from "react";
 
 function Service() {
   const [tip, setTip] = useState(5);
+
   return (
     <div>
       <span>How did you like your service?</span>
-      <select value={tip}>
+      <select value={tip} onChange={(e) => setTip(e.target.value)}>
         <option value={0}> Dissatisfied (0%)</option>
         <option value={5}> It was okay (5%)</option>
         <option value={10}> It was good (10%)</option>
