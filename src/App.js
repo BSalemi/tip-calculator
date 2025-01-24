ff05c31c912d70c3c6e86fa96b4f0fd7d63c1d62
BSalemi <briandsalemi@gmail.com>
2025-01-24 07:47:29 -0500

    changing the name of state to better reflect what it holds, added a new set of state for the second SelectPercentage component

--- src/App.js
+++ src/App.js
@@ -6,22 +6,30 @@ import Total from "./Total.js";
 
 function App() {
   const [price, setPrice] = useState(0);
-  const [tip, setTip] = useState(0);
+  const [tipPercentage1, setTipPercentage1] = useState(0);
+  const [tipPercentage2, setTipPercentage2] = useState(0);
 
   function handleReset() {
     setPrice(0);
-    setTip(0);
+    setTipPercentage1(0);
+    setTipPercentage2(0);
   }
   return (
     <div>
       <BillInput price={price} setPrice={setPrice} />
-      <SelectPercentage tip={tip} setTip={setTip}>
+      <SelectPercentage
+        tipPercentage={tipPercentage1}
+        setTipPercentage={setTipPercentage1}
+      >
         <span>How did you like the service?</span>
       </SelectPercentage>
-      <SelectPercentage tip={tip} setTip={setTip}>
+      <SelectPercentage
+        tipPercentage={tipPercentage2}
+        setTipPercentage={setTipPercentage2}
+      >
         <span>How did your friend like the service?</span>
       </SelectPercentage>
-      <Total tip={tip} bill={price} />
+      <Total tipPercentage={tipPercentage1} bill={price} />
       <Reset handleReset={handleReset} />
     </div>
   );
