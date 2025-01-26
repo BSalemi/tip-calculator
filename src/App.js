34c8e8d5bce7471af1ffa96a98f4b12789d54fc4
BSalemi <briandsalemi@gmail.com>
2025-01-26 08:08:55 -0500

    Created a parent component TipCalculator and made the other components the child components

--- src/App.js
+++ src/App.js
@@ -1,46 +1,7 @@
-import { useState } from "react";
-import BillInput from "./BillInput.js";
-import SelectPercentage from "./SelectPercentage.js";
-import Reset from "./ResetButton.js";
-import Total from "./Total.js";
+import TipCalculator from "./TipCalculator.js";
 
 function App() {
-  const [bill, setBill] = useState("");
-  const [tipPercentage1, setTipPercentage1] = useState(0);
-  const [tipPercentage2, setTipPercentage2] = useState(0);
-
-  let combinedTip = (tipPercentage1 + tipPercentage2) / 2;
-
-  function handleReset() {
-    setBill("");
-    setTipPercentage1(0);
-    setTipPercentage2(0);
-  }
-
-  return (
-    <div>
-      <BillInput bill={bill} setBill={setBill} />
-      <SelectPercentage
-        tipPercentage={tipPercentage1}
-        setTipPercentage={setTipPercentage1}
-      >
-        <span>How did you like the service?</span>
-      </SelectPercentage>
-      <SelectPercentage
-        tipPercentage={tipPercentage2}
-        setTipPercentage={setTipPercentage2}
-      >
-        <span>How did your friend like the service?</span>
-      </SelectPercentage>
-
-      {bill > 0 && (
-        <>
-          <Total tip={combinedTip} bill={bill} />
-          <Reset handleReset={handleReset} />
-        </>
-      )}
-    </div>
-  );
+  return <TipCalculator />;
 }
 
 export default App;
